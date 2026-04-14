54465ce290bf532d2e4f0155af9d33aae672876d
omercnsz <omercnsz@users.noreply.github.com>
2026-04-14 03:00:11 +0300

    chore: v19.9.8 Atomic Desync Final release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import java.util.Properties
 import java.io.FileInputStream
 
 group = "com.truva"
-version = "19.9.7"
+version = "19.9.8"
 
 android {
     namespace = "com.truva"
@@ -18,8 +18,8 @@ android {
         applicationId = "com.truva"
         minSdk = 24
         targetSdk = 34
-        versionCode = 53
-        versionName = "19.9.7"
+        versionCode = 54
+        versionName = "19.9.8"
     }
 
     testOptions {
